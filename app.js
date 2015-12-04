889847608c6f1b55a9dadd0a018e19b53bd91b64
reFucktor <hmvalcarcel@gmail.com>
2015-12-04 10:05:09 -0500

    log to know the enviroment

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/materialize', express.static(__dirname + '/node_modules/materialize-cs
 // view engine setup ===============================================================
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+console.log(app.get('env'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
